a7f639186006bdf0f42e5f131bf49f740655e1fa
DaLegendaryCoder13 <86676894+DaLegendaryCoder13@users.noreply.github.com>
2021-07-07 20:54:18 -0500

    Add files via upload

---
+++ Control Flow Excercise.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+let minutes = 3
+
+for Timer in 0...minutes
+{
+    print("\(Timer) minutes")
+    
+    if Timer == minutes {
+        print ("YOU BETTER WAKE UP!")
+    }
+}
+
+
+
